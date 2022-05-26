3f4c0b5fe6ee3739c9e5034eddef1d638554c3bf
Aleksei Gutikov <gutikoff@gmail.com>
2022-05-26 04:49:40 +0300

    move submodules

--- src/min_max.cpp
+++ src/min_max.cpp
@@ -1,17 +1,11 @@
-#include <algorithm>
-#include <ostream>
-#include <vector>
-#include <random>
-#include <functional>
-#include <limits>
-#include <numeric>
-#include <iostream>
 #include <chrono>
 #include <concepts>
+#include <vector>
 
 #include <benchmark/benchmark.h>
 
 #include "pp_product.h"
+#include "generate.h"
 
 
 // Min and Max implementations:
@@ -111,79 +105,6 @@ struct StatsConsumer
         }
     }
 };
-
-
-
-
-template<typename T>
-struct uniform_dist
-{};
-
-template<std::integral T>
-struct uniform_dist<T>
-{
-    using type = std::uniform_int_distribution<T>;
-};
-
-template<std::floating_point T>
-struct uniform_dist<T>
-{
-    using type = std::uniform_real_distribution<T>;
-};
-
-static std::random_device rand_dev;
-
-template<typename T>
-struct random_v
-{
-    static std::vector<T> generate(size_t size)
-    {
-        T v_min = std::numeric_limits<T>::min();
-        T v_max = std::numeric_limits<T>::max();
-        std::default_random_engine e1(rand_dev());
-        typename uniform_dist<T>::type dist(v_min, v_max);
-        std::vector<T> v(size);
-        std::generate(v.begin(), v.end(), [&e1, &dist]() -> T { return dist(e1); });
-        return v;
-    }
-};
-
-
-template<typename T>
-struct growing_v
-{
-    static std::vector<T> generate(size_t size)
-    {
-        std::vector<T> v(size);
-        T start = 0;
-        std::iota(v.begin(), v.end(), start);
-        return v;
-    }
-};
-
-template<typename T>
-struct falling_v
-{
-    static std::vector<T> generate(size_t size)
-    {
-        std::vector<T> v(size);
-        T start = std::numeric_limits<T>::max() / 2;
-        std::generate(v.begin(), v.end(), [start]() mutable -> T { return start--; });
-        return v;
-    }
-};
-
-template<typename T>
-struct const_v
-{
-    static std::vector<T> generate(size_t size)
-    {
-        std::vector<T> v(size);
-        std::fill(v.begin(), v.end(), 0);
-        return v;
-    }
-};
-
 
 
 template<typename T, template<typename> class Src, template<typename> class MinMax>
